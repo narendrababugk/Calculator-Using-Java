86a1868c215de11222203b6ab24ee0f0a09cc73b
Narendrababu GK <narendrababugk@gmail.com>
2026-09-23 18:53:44 +0530

    Update methods.java

--- methods.java
+++ methods.java
@@ -1,21 +1,28 @@
 class method{
 	double res=0;
-	
+
+	//Addition
 	void add(double val){
 		res+=val;
 		System.out.print("\nResult:"+res);
 		System.out.println("\n");
 	}
+
+	//Substraction
 	void sub(double val){
 		res-=val;
 		System.out.print("\nResult:"+res);
 		System.out.println("\n");
 	}
+
+	//Multiplication
 	void mul(double val){
 		res*=val;
 		System.out.print("\nResult:"+res);
 		System.out.println("\n");
 	}
+
+	//Division
 	void div(double val){
 		if(res==0 || val==0){
 			System.out.print("Division is not possible");
@@ -26,6 +33,8 @@ class method{
 		System.out.println("\n");
 		}
 	}
+
+	//Modulus
 	void mod(double val){
 		if(res==0 || val==0){
 			System.out.print("Modulus is not possible");
@@ -36,6 +45,8 @@ class method{
 		System.out.println("\n");
 		}
 	}
+
+	//Clear the old answer
 	void clear(double val){
 		if(val==01.0){
 			res=0;
